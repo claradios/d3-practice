4ed4d600fa2324a790bf2857fc16d4d904e18578
Clara Dios <clara.dios.diez@gmail.com>
2020-04-08 20:57:44 +0200

    represent building height from data json

--- 2.05.0/js/main.js
+++ 2.05.0/js/main.js
@@ -3,34 +3,30 @@
 *    Mastering Data Visualization with D3.js
 *    2.5 - Activity: Adding SVGs to the screen
 */
-const data = [25,20,10,12,15]
-const svg = d3.select("#chart-area").append("svg")
-    .attr("width",400)
-    .attr("height",400)
 
-// const circle= svg.append("circle")
-//     .attr("cx",200)
-//     .attr("cy",200)
-//     .attr("r",100)
-//     .attr("fill","blue")
+d3.json("data/buildings.json")
+    .then((data) => {
 
-// const rectangle = svg.append("rect")
-//     .attr("x",5)
-//     .attr("y",5)
-//     .attr("width",30)
-//     .attr("height",30)
-//     .attr("fill","aquamarine")
+        data.forEach((d) => {
+            d.height = +d.height;
+        });
 
-const dataCircles = svg.selectAll("circle")
-    .data(data)
+        const svg = d3.select("#chart-area").append("svg")
+            .attr("width", 800)
+            .attr("height", 800)
 
-    dataCircles.enter()
-        .append("circle")
-            .attr("cx",(d,i)=> {
-                return (i * 50) + 25;
+        const dataCircles = svg.selectAll("circle")
+            .data(data)
+
+        dataCircles.enter()
+            .append("circle")
+            .attr("cx", (d, i) => {
+                return (i * 100) + 200;
             })
-            .attr("cy",200)
-            .attr("r",(d)=> {
-                return d;
+            .attr("cy", 200)
+            .attr("r", (d) => {
+                console.log(d.name)
+                return d.height /7;
             })
-            .attr("fill","red")
+            .attr("fill", "red")
+    });
