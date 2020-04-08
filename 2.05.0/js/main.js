61217f69a123ce08d9dbcce10d74d05ca083333a
Clara Dios <clara.dios.diez@gmail.com>
2020-04-08 21:04:51 +0200

    bar diagram exercise completed

--- 2.05.0/js/main.js
+++ 2.05.0/js/main.js
@@ -14,19 +14,32 @@ d3.json("data/buildings.json")
         const svg = d3.select("#chart-area").append("svg")
             .attr("width", 800)
             .attr("height", 800)
+        
+        const rectangles = svg.selectAll("rectangles").data(data)
 
-        const dataCircles = svg.selectAll("circle")
-            .data(data)
+        rectangles.enter()
+            .append("rect")
+            .attr("x",(d,i)=> {
+                return(i*50) +100
+            })
+            .attr("y",5)
+            .attr("height",(d)=> {
+                return d.height
+            })
+            .attr("width",30)
 
-        dataCircles.enter()
-            .append("circle")
-            .attr("cx", (d, i) => {
-                return (i * 100) + 200;
-            })
-            .attr("cy", 200)
-            .attr("r", (d) => {
-                console.log(d.name)
-                return d.height /7;
-            })
-            .attr("fill", "red")
+        // const dataCircles = svg.selectAll("circle")
+        //     .data(data)
+
+        // dataCircles.enter()
+        //     .append("circle")
+        //     .attr("cx", (d, i) => {
+        //         return (i * 100) + 200;
+        //     })
+        //     .attr("cy", 200)
+        //     .attr("r", (d) => {
+        //         console.log(d.name)
+        //         return d.height /7;
+        //     })
+        //     .attr("fill", "red")
     });
